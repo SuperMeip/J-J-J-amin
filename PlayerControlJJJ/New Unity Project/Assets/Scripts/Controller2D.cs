15731a650fc283184d4df48b1e37e85179e2f421
Meep <supermeip@gmail.com>
2015-09-17 13:12:48 -0400

    Player Controller Uploaded
    upload of player controler v2

---
+++ PlayerControlJJJ/New Unity Project/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+	BoxCollider2D collider;
+	//Holds the corners of the player
+	RaycastPoints corner;
+	//How wide the 'skin' is for raycasting
+	public LayerMask collisionMask;
+	const float skinWidth = 0.015f;
+	//The number of rays being cast in each direction
+	public int raysHotizontal = 4;
+	public int raysVerticle = 4;
+	//Stores directions of collissions! This is very useful
+	public CollisionInfo collisions;
+	//Used to hold the spacing of the rays being cast in each direction
+	float raySpacingVerticle;
+	float raySpacingHorizontal;
+	
+	void Start () 
+	{
+		collider = GetComponent<BoxCollider2D> ();
+		CalculateRaySpacing ();
+	}
+	
+	/// <summary>
+	/// Move the specified velocity.
+	/// </summary>
+	/// <param name="velocity">Velocity.</param>
+	public void Move(Vector3 velocity)
+	{
+		//update cast points when you go to move
+		UpdateRaycastPoints ();
+		collisions.Reset ();
+		//check if you're going to hit anything vertically and sets verticle velocity appropriately
+		if (velocity.y!= 0)
+			VerticalCollisions (ref velocity);
+		if (velocity.x != 0)
+			HorizontalCollisions (ref velocity);
+
+		transform.Translate (velocity);
+	}
+	/// <summary>
+	/// Actvates on a horizontal collision, prevents phasing.
+	/// *draws veriticle rays for debug
+	/// <param name="velocity">pass by reference the velocity of the player's current movement, edits it to move to edge if an edge exists</param>
+	/// </summary>
+	void HorizontalCollisions(ref Vector3 velocity)
+	{
+		float directionX = Mathf.Sign (velocity.x);
+		float rayLength = Mathf.Abs (velocity.x) + skinWidth;
+		for (int i = 0; i < raysHotizontal; i++) 
+		{
+			Vector2 rayOrigin = (directionX == -1)?corner.bottomLeft:corner.bottomRight;
+			rayOrigin += Vector2.up * (raySpacingHorizontal*i);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+			Debug.DrawRay(rayOrigin, Vector2.right * directionX *rayLength, Color.red);
+			if (hit)
+			{
+				velocity.x = (hit.distance - skinWidth) * directionX;
+				rayLength = hit.distance;
+				collisions.left = directionX == -1;
+				collisions.right = directionX == 1;
+			}
+		}
+	}
+	/// <summary>
+	/// Actvates on a Vertical collision, prevents phasing.
+	/// *draws veriticle rays for debug
+	/// <param name="velocity">pass by reference the velocity of the player's current movement, edits it to move to edge if an edge exists</param>
+	/// </summary>
+	void VerticalCollisions(ref Vector3 velocity)
+	{
+		float directionY = Mathf.Sign (velocity.y);
+		float rayLength = Mathf.Abs (velocity.y) + skinWidth;
+		for (int i = 0; i < raysVerticle; i++) 
+		{
+			Vector2 rayOrigin = (directionY == -1)?corner.bottomLeft:corner.topLeft;
+			rayOrigin += Vector2.right * (raySpacingVerticle*i + velocity.x);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+			Debug.DrawRay(rayOrigin, Vector2.up * directionY *rayLength, Color.red);
+			if (hit)
+			{
+				velocity.y = (hit.distance - skinWidth) * directionY;
+				rayLength = hit.distance;
+				collisions.below = directionY == -1;
+				collisions.above = directionY == 1;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Holds the corners of the player for raycasting
+	/// </summary>
+	struct RaycastPoints
+	{
+		public Vector2 topLeft, topRight, bottomLeft, bottomRight;
+	}
+
+	///<summary>
+	/// Stores info detailing what side is colliding
+	/// </summary>
+	public struct CollisionInfo
+	{
+		public bool above,below,left,right;
+		public void Reset() 
+		{
+			above = below = left = right = false;
+		}
+	}
+	/// <summary>
+	/// Updates the raycast points of origin on the skin.
+	/// use when the player moves
+	/// </summary>
+	void UpdateRaycastPoints()
+	{
+		Bounds bounds = collider.bounds;
+		bounds.Expand (skinWidth * -2);
+		corner.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+		corner.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+		corner.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+		corner.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+	}
+	/// <summary>
+	/// Calculates the spacing of the rays.
+	/// </summary>
+	void CalculateRaySpacing()
+	{
+		Bounds bounds = collider.bounds;
+		bounds.Expand (skinWidth * -2);
+		raysHotizontal = Mathf.Clamp (raysHotizontal, 2, int.MaxValue);
+		raysVerticle = Mathf.Clamp (raysVerticle, 2, int.MaxValue);
+		raySpacingHorizontal = bounds.size.y / (raysHotizontal - 1);
+		raySpacingVerticle = bounds.size.x / (raysVerticle - 1);
+
+	}
+}
---
+++ PlayerControlJJJ/New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+
+	Controller2D controller;
+	//Sprites
+	public Sprite leftSpriteAir;
+	public Sprite rightSpriteAir;
+	public Sprite leftSprite;
+	public Sprite rightSprite;
+	//player facing info
+	bool inAir, left;
+	//physics values
+	float gravity;
+	float jumpVelocity;
+	//editable settings
+	public float jumpHeight = 2.7f;
+	public float jumpTime = 0.5f;
+	public float moveSpeed = 6;
+	//smoothers
+	float airAccelerationTime = 0.2f;
+	float groundAccelerationTime = 0.1f;
+	float velocitySmoothingX;
+	//velocity!
+	Vector3 velocity;
+
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+		left = true;
+		//calculate the physics values based on the settings
+		gravity = -(2 * jumpHeight) / Mathf.Pow (jumpTime, 2);
+		jumpVelocity = Mathf.Abs (gravity) * jumpTime;
+	}
+
+	void Update () {
+		//Sprite Management
+		if (controller.collisions.below)//Checks when lands to turn off jump sprite
+			inAir = false;
+		if (inAir) //If in air
+		{
+			if (velocity.x < 0){ //If moving left
+				GetComponent<SpriteRenderer> ().sprite = leftSpriteAir;
+				left = true;
+			}
+			else if (velocity.x > 0){ //If moving right
+				GetComponent<SpriteRenderer> ().sprite = rightSpriteAir;
+				left = false;
+			}
+			else
+			{
+				if (left)
+					GetComponent<SpriteRenderer> ().sprite = leftSpriteAir;
+				else
+					GetComponent<SpriteRenderer> ().sprite = rightSpriteAir;
+			}
+			
+		} 
+		else //If on ground
+		{
+			if (velocity.x < 0){
+				GetComponent<SpriteRenderer> ().sprite = leftSprite;
+				left = true;
+			}
+			else if (velocity.x > 0){
+				GetComponent<SpriteRenderer> ().sprite = rightSprite;
+				left = false;
+			}
+			else
+			{
+				if (left)
+					GetComponent<SpriteRenderer> ().sprite = leftSprite;
+				else
+					GetComponent<SpriteRenderer> ().sprite = rightSprite;
+			}
+		}		
+		//reset gravity if touching the ground
+		if (controller.collisions.above || controller.collisions.below) 
+		{
+			velocity.y = 0;
+		}
+		//prevents climbing up walls (Temporary? still a bit buggy)
+		if (controller.collisions.left || controller.collisions.right) 
+		{
+			velocity.x = 0;
+		}
+		//jump
+		if (Input.GetKeyDown (KeyCode.Space) && controller.collisions.below)
+		{
+			velocity.y = jumpVelocity;
+			inAir = true; //Used for jump sprite
+		}
+		//movement
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+		float smoothToVelocity = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp(velocity.x,smoothToVelocity, ref velocitySmoothingX, (controller.collisions.below?groundAccelerationTime:airAccelerationTime));
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
